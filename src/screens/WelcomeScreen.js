713f405811a724ff19bfceeb3e8a545b491da8c6
Nguyen Dang Duc <Nguyen Dang Duc>
2023-08-01 13:42:28 +0700

    update src

---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,33 @@
+import { useNavigation } from '@react-navigation/native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import React from 'react';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
+export default function WelcomeScreen() {
+
+  const navigation = useNavigation();
+
+  return (
+    <View className="flex-1 flex justify-around bg-white">
+      <View className="space-y-2">
+            <Text style={{ fontSize: wp(10) }} className="text-center font-bold text-gray-700 text-4xl">
+              Jarvis
+            </Text>
+            <Text style={{ fontSize: wp(4) }} className="text-center text-gray-600 font-semibold">
+              The Future is here, powered by AI
+            </Text>
+      </View>
+      <View className="flex-row justify-center">
+        <Image source={require('../../assets/images/img_welcome.png')}  style={{ width: wp(80), height: hp(30) }}/>
+      </View>
+      <TouchableOpacity 
+      className="bg-blue-500 mx-5 p-4 rounded-2xl"
+      onPress={() => navigation.navigate('Home')}
+      >
+        <Text className="text-center font-bold text-white text-2xl">
+          Get Started
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
